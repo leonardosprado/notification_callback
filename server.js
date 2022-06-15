4edcb5a4e590f029d25f3fa4d829f347e0dccd6e
Leonardo <leonardo.prado@soluti.com.br>
2022-06-15 08:14:38 -0300

    Criação de Log

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 const express = require("express")
+var fs = require("fs");
 const app = express()
-const port = 8080
+const port = process.env.PORT || 3000
 app.get('/', (req, res) => {
-  res.send(`URL de callback para notificação, o sistema irá notificar quando o processo de assinatura for finalizado, será feito uma requisição 
-  HTTP(s) - POST na URL informada, na chamada será concatenado o código transação no final da url informada neste parâmetro.`)
+
+    fs.readFile('log.txt', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        res.send(data);
+        console.log(data);
+      });
+      
+//   res.send(`URL de callback para notificação, o sistema irá notificar quando o processo de assinatura for finalizado, será feito uma requisição 
+//   HTTP(s) - POST na URL informada, na chamada será concatenado o código transação no final da url informada neste parâmetro.`)
 })
 app.post('/notification_callback',(req,res)=>{
     var tcn = req.query.tcn;
+    var logger = fs.createWriteStream('log.txt')
+    logger.write(`${new Date()}:::${tcn}`) // append string to your file
     console.log(tcn);
     res.status(200);
     res.send({
